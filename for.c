dd5c04f48d3b8f3356987de70891893e13857021
suansh25 <kurakulasuansh@gmail.com>
2023-01-16 17:35:45 +0530

    for loop incomplete

--- for.c
+++ for.c
@@ -1,20 +1,20 @@
-#include<stdio.h>
-int main(){
-    int a,b,i;
-    scanf("%d%d",&a,&b);
-    for(a=0;a<10;a++){
-     if(a=1){printf("one");}
-     else if(a=2)printf("two");
-     else if(a=3)printf("three");
-     else if(a=4)printf("four");
-     else if(a=5)printf("five");
-     else if(a=6)printf("six");
-     else if(a=7)printf("seven");
-     else if(a=8)printf("eight");
-     else if(a=9)printf("nine");
-    }
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+
+
+int main() 
+{
+    int a, b,i;
+    scanf("%d\n%d", &a, &b);
+  	// Complete the code.
+      for(i=0;i<10;i++)
+      if(a%2==0)printf("even\n");
+      else printf("odd\n");
+      if(b%2==0)printf("even\n");
+      else printf("odd\n");
     return 0;
 }
-
 
