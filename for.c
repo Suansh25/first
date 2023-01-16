a55831988db21b4338245d0ca0b26d53acd78339
suansh25 <kurakulasuansh@gmail.com>
2023-01-16 17:33:40 +0530

    for loop incomplete model

--- for.c
+++ for.c
@@ -1,1 +1,20 @@
-hello world
+#include<stdio.h>
+int main(){
+    int a,b,i;
+    scanf("%d%d",&a,&b);
+    for(a=0;a<10;a++){
+     if(a=1){printf("one");}
+     else if(a=2)printf("two");
+     else if(a=3)printf("three");
+     else if(a=4)printf("four");
+     else if(a=5)printf("five");
+     else if(a=6)printf("six");
+     else if(a=7)printf("seven");
+     else if(a=8)printf("eight");
+     else if(a=9)printf("nine");
+    }
+
+    return 0;
+}
+
+
